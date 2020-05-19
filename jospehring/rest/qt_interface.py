3522e2080cd4201e6ec64a91efb15f7a80785d21
jiuyoupp <529391453@qq.com>
2020-05-19 16:41:01 +0800

    将子窗口和主窗口的数据传递改为信号和槽的方式

--- jospehring/rest/qt_interface.py
+++ jospehring/rest/qt_interface.py
@@ -2,6 +2,7 @@ from jospehring.usecase.interface import Interface
 from jospehring.domain.ring import Ring
 from jospehring.usecase.reader_use_case import read_data
 from PySide2.QtWidgets import QApplication, QWidget, QDialog
+from PySide2.QtCore import Signal
 from jospehring.rest.ui_main import Ui_Form
 from jospehring.rest.child import Ui_Dialog
 import sys
@@ -30,33 +31,33 @@ class MyMainWindow(QWidget, Ui_Form, Interface):
         print('退出本程序！')
         sys.exit(0)
 
-    def reset(self):
-
+    def opendialog(self):
         dialog = ChildWindow(self)
-        dialog.comboBox.clear()
+        dialog.Signal_OneParameter.connect(self.deal_emit_slot)
+        dialog.show()
         dialog.exec_()
-        step, location = dialog.reset_data()
-        step = int(step)
-        location = int(location)
         dialog.destroy()
-        self.jospeh.reset(step, location)
+
+    def reset(self):
+        if self.jospeh:
+            self.opendialog()
 
     def show_menu(self):
         for index, item in enumerate(self.option_list, 1):
             print(index, item[0])
 
+    def deal_emit_slot(self, step, location, path):
+        step = int(step)
+        location = int(location)
+        reader = read_data(path, 'r')
+        print(step)
+        self.jospeh.from_reader(reader)
+        self.jospeh.reset(step, location)
+
     def create_jospehring(self, obj=Ring, start=0, location=0):
         self.jospeh = obj()
         if self.jospeh:
-            dialog = ChildWindow(self)
-            dialog.exec_()
-            step, location, path = dialog.get_data()
-            step = int(step)
-            location = int(location)
-            dialog.destroy()
-            reader = read_data(path, 'r')
-            self.jospeh.from_reader(reader)
-            self.jospeh.reset(step, location)
+            self.opendialog()
 
     def next_people(self):
         outperson = self.jospeh.next()
@@ -92,23 +93,24 @@ class MyMainWindow(QWidget, Ui_Form, Interface):
 
 
 class ChildWindow(QDialog, Ui_Dialog):
+    Signal_OneParameter = Signal(str, str, str)
+
     def __init__(self, parent=None):
         super(ChildWindow, self).__init__(parent)
         self.setupUi(self)
+        self.buttonBox.accepted.connect(self.emit_signal)
 
-    def get_data(self):
+    def emit_signal(self):
         text1 = self.lineEdit.text()
         text2 = self.lineEdit_2.text()
         path = self.comboBox.currentText()
-        return text1, text2, path
+        self.Signal_OneParameter.emit(text1, text2, path)
 
-    def reset_data(self):
-        text1 = self.lineEdit.text()
-        text2 = self.lineEdit_2.text()
-        return text1, text2
+
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     obj = MyMainWindow()
     obj.show()
     sys.exit(app.exec_())
+

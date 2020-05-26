eb49f8cc0ecedf2de0c4b815e1a59fb299bbd2d7
jiuyoupp <529391453@qq.com>
2020-05-26 17:46:48 +0800

    修改mock文件

--- tests/adapter/test_interface.py
+++ tests/adapter/test_interface.py
@@ -1,17 +1,16 @@
 from jospehring.usecase.ring import Ring
 from jospehring.domain.person import Person
-from jospehring.adapter.reader import read_data
+
 from unittest import mock
 from jospehring.adapter.interface import Interface
 import unittest
 
 
 class TestRingFromReader(unittest.TestCase):
-    @mock.patch('jospehring.adapter.reader.read_data')
-    def test_txtreader(self, mock_txtreader):
-        mock_txtreader.return_value = [Person('冯巧', 1), Person('昌瑶灵', 2), Person('袁春绿', 3)]
+    @mock.patch('jospehring.adapter.interface.ReadData')
+    def test_txtreader(self, mock_case):
+        mock_case.read_data.return_value = [Person('冯巧', 1), Person('昌瑶灵', 2), Person('袁春绿', 3)]
         interface = Interface()
-        interface.create_jospehring1()
-
-        assert interface.jospeh._ring[0].name == '冯巧'
-
+        interface.create_jospehring('')
+        assert interface.jospeh.ring[0].name == '冯巧'
+        assert interface.jospeh.ring[1].name == '昌瑶灵'
